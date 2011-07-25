44d190a73d6848bdcdffbc554c832f86e593de5e
Radu Ioan Fericean <rif@love.ro>
2011-07-25 22:24:35 +0300

    user and password from cli

--- webservice_test.py
+++ webservice_test.py
@@ -1,8 +1,12 @@
 import httplib2
 import simplejson as json
+import sys
+
+USER = sys.argv[1] if len(sys.argv) == 3 else 'demows'
+PASSWORD = sys.argv[2] if len(sys.argv) == 3 else 'deadbeef2'
 
 http = httplib2.Http()
-response,content = http.request("http://portal.safefleet.eu/safefleet/webservice/authenticate/?username=demows&password=deadbeef2", 'POST')
+response,content = http.request("http://portal.safefleet.eu/safefleet/webservice/authenticate/?username=%(user)s&password=%(pass)s" % {'user': USER, 'pass': PASSWORD}, 'POST')
 
 print "Athentication response:"
 print response
@@ -18,14 +22,18 @@ print '-'*100
 
 response,content = http.request("http://portal.safefleet.eu/safefleet/webservice/get_vehicles/", headers=headers)
 
-print "get_companies response:"
+print "get_vehicles response:"
 arr =  json.loads(content)
+count = 0
 for obj in arr:
-    print obj
+    count += 1
+    print obj, 
     r,c = http.request("http://portal.safefleet.eu/safefleet/webservice/get_vehicle_dynamic_info/?vehicle_id=" + str(obj['vehicle_id']), headers=headers)
     a =  json.loads(c)
-    print a
+    print "***** get_vehicle_dynamic_info:", a
 print '-'*100
+
+print "Total: %d vehicles." % count
 
 # urllib2 opener test
 #import urllib2
